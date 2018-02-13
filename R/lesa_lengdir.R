ea63a7c4d69f917c4338e735ef4b063d6f484450
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2018-02-13 11:59:20 +0000

    fix geoconvert, need oraF.sql internally in xe

--- R/lesa_lengdir.R
+++ R/lesa_lengdir.R
@@ -8,7 +8,31 @@
 #'
 lesa_lengdir <- function(con) {
 
-  mar::lesa_lengdir(con) %>%
-    dplyr::union(mar::tbl_mar(con, "hafvog.lengdir"))
+  d1 <-
+    mar::tbl_mar(con, "fiskar.lengdir") %>%
+    dplyr::select(-c(snn:sbt)) %>%
+    right_join(mar::tbl_mar(con, "fiskar.stodvar") %>%
+                 dplyr::mutate(ar = to_number(to_char(dags, "YYYY"))) %>%
+                 dplyr::filter(ar < 2017) %>%
+                 dplyr::select(synis_id), by = "synis_id") %>%
+    left_join(mar::tbl_mar(con, "fiskar.numer") %>%
+                dplyr::mutate(r = ifelse(fj_talid==0 | is.na(fj_talid), 1, 1 + (fj_talid / ifelse(fj_maelt == 0 | is.na(fj_talid), 1, fj_maelt)))) %>%
+                dplyr::select(synis_id, tegund, r), by = c("synis_id", "tegund")) %>%
+    mutate(source = "fiskar")
+
+
+  d2 <-
+    mar::tbl_mar(con, "hafvog.lengdir") %>%
+    right_join(mar::tbl_mar(con, "hafvog.stodvar") %>%
+                 dplyr::mutate(ar = to_number(to_char(dags, "YYYY"))) %>%
+                 dplyr::filter(ar == 2017) %>%
+                 dplyr::select(synis_id), by = "synis_id") %>%
+    left_join(mar::tbl_mar(con, "hafvog.numer") %>%
+                dplyr::mutate(r = ifelse(fj_talid==0 | is.na(fj_talid), 1, 1 + (fj_talid / ifelse(fj_maelt == 0 | is.na(fj_talid), 1, fj_maelt)))) %>%
+                dplyr::select(synis_id, tegund, r), by = c("synis_id", "tegund")) %>%
+    mutate(source = "hafvog")
+
+  d1 %>%
+    dplyr::union(d2)
 
 }
--- R/lesa_stodvar.R
+++ R/lesa_stodvar.R
@@ -1,6 +1,6 @@
 #' Title
 #'
-#' Some text
+#' The current form is just mimicry
 #'
 #' @param con XX
 #'
@@ -8,10 +8,32 @@
 #'
 lesa_stodvar <- function(con) {
 
-  mar::tbl_mar(con, "fiskar.stodvar") %>%
-    dplyr::select(synis_id:heildarafli) %>%
-    dplyr::union(mar::tbl_mar(con, "hafvog.stodvar") %>%
-                   dplyr::select(synis_id:heildarafli)) %>%
-    mutate(ar = to_number(to_char(dags, "YYYY")))
+  d1 <-
+    mar::tbl_mar(con, "fiskar.stodvar") %>%
+    dplyr::select(synis_id:heildarafli, synaflokkur) %>%
+    dplyr::mutate(ar = to_number(to_char(dags, "YYYY"))) %>%
+    #dplyr::filter(synaflokkur == 30, ar < 2017) %>%
+    dplyr::left_join(mar::tbl_mar(con, "fiskar.togstodvar") %>%
+                       dplyr::select(synis_id, toglengd, tognumer), by = "synis_id") %>%
+    dplyr::mutate(source = "fiskar")
+
+  d2 <-
+    mar::tbl_mar(con, "hafvog.stodvar") %>%
+    dplyr::select(synis_id:heildarafli, synaflokkur) %>%
+    dplyr::mutate(ar = to_number(to_char(dags, "YYYY"))) %>%
+    #dplyr::filter(synaflokkur == 30, ar == 2017) %>%
+    dplyr::left_join(mar::tbl_mar(con, "hafvog.togstodvar") %>%
+                dplyr::select(synis_id, toglengd, tognumer), by = "synis_id") %>%
+    dplyr::mutate(source = "hafvog")
+
+  d1 %>%
+    dplyr::union(d2) %>%
+    rename(lon1 = kastad_v_lengd,
+           lon2 = hift_v_lengd,
+           lat1 = kastad_n_breidd,
+           lat2 = hift_n_breidd) %>%
+    mar:::geoconvert(col.names = c("lat1", "lon1")) %>%
+    mar:::geoconvert(col.names = c("lat2", "lon2")) %>%
+    dplyr::mutate(index = reitur * 100 + tognumer)
 
 }
