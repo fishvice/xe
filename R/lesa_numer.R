78ad7782c13f148f823e4ecdb458944b37e1b233
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2018-10-07 13:15:39 +0000

    dplyr::if_else cause problems, revert using only if_else

--- R/lesa_numer.R
+++ R/lesa_numer.R
@@ -12,12 +12,12 @@ lesa_numer <- function(con, schema = "fiskar") {
   tbl_mar(con, paste0(schema, ".numer")) %>%
     dplyr::select(synis_id, tegund, fj_maelt, fj_talid, fj_kyngreint,
                   fj_vigtad, fj_magasyna) %>%
-    dplyr::mutate(synis_id = dplyr::if_else(schema == "fiskar", synis_id, -synis_id),
-                  fj_maelt = dplyr::if_else(is.na(fj_maelt), 0, fj_maelt),
-                  fj_talid = dplyr::if_else(is.na(fj_talid), 0, fj_talid),
-                  fj_kyngreint = dplyr::if_else(is.na(fj_kyngreint), 0, fj_kyngreint),
-                  fj_vigtad = dplyr::if_else(is.na(fj_vigtad), 0, fj_vigtad),
-                  fj_magasyna = dplyr::if_else(is.na(fj_magasyna), 0, fj_vigtad),
+    dplyr::mutate(synis_id = if_else(schema == "fiskar", synis_id, -synis_id),
+                  fj_maelt = if_else(is.na(fj_maelt), 0, fj_maelt),
+                  fj_talid = if_else(is.na(fj_talid), 0, fj_talid),
+                  fj_kyngreint = if_else(is.na(fj_kyngreint), 0, fj_kyngreint),
+                  fj_vigtad = if_else(is.na(fj_vigtad), 0, fj_vigtad),
+                  fj_magasyna = if_else(is.na(fj_magasyna), 0, fj_vigtad),
                   fj_alls = fj_maelt + fj_talid)
 
 }
