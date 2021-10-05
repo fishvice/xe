da140e3c9637c0e0b4890f9d5807bc2f26484d92
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2021-10-05 12:12:31 +0000

    if_else does not need ::

--- R/lesa_lengdir.R
+++ R/lesa_lengdir.R
@@ -11,6 +11,6 @@ lesa_lengdir <- function(con, schema = "fiskar") {
 
   tbl_mar(con, paste0(schema, ".lengdir")) %>%
     dplyr::select(synis_id:kynthroski) %>%
-    dplyr::mutate(synis_id = dplyr::if_else(schema == "fiskar", synis_id, -synis_id))
+    dplyr::mutate(synis_id = if_else(schema == "fiskar", synis_id, -synis_id))
 
 }
