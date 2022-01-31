6cc82ae6884dd1a92c5b4d64befcfabcf40d7edf
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2022-01-31 20:28:49 +0000

    pkgdown - more stuff

---
+++ einarhj_webupdate.R
@@ -0,0 +1,3 @@
+system("rm -rf  /net/www/export/home/hafri/einarhj/public_html/xe/*")
+system("cp -r docs/* /net/www/export/home/hafri/einarhj/public_html/xe/.")
+system("chmod -R a+rX /net/www/export/home/hafri/einarhj/public_html/ramb")
