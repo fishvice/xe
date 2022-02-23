51f0e99ec8262c24b9240713625e88fb4f4398ae
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2022-02-23 12:20:26 +0000

    documents

--- einarhj_webupdate.R
+++ einarhj_webupdate.R
@@ -1,5 +1,6 @@
-devtools::build_readme()
+devtools::build_readme() # not working on windose
 pkgdown::build_site()
+# run this on linux
 system("rm -rf  /net/www/export/home/hafri/einarhj/public_html/xe/*")
 system("cp -r docs/* /net/www/export/home/hafri/einarhj/public_html/xe/.")
 system("chmod -R a+rX /net/www/export/home/hafri/einarhj/public_html/ramb")
